dc99ef74152ba275c34b2db872cbaa3c2f8c4e16
rjmithcell92 <robertjames.mitchell@phs.scot>
2025-02-21 14:54:05 +0000

    Shortened Code for Main UI Script 2 for greater efficiency.

--- inst/disclosure_control/2_UI_Scripts/2_data_input.R
+++ inst/disclosure_control/2_UI_Scripts/2_data_input.R
@@ -4,46 +4,27 @@
 #Last updated: 02 Oct 2024
 #By: Robert Mitchell
 #Script: 2_data_input.R
-#Purpose: To set up UI for the Main Data Input Tab
+#Purpose: Set up UI for the Main Tab used for uploading/processing input data
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
 
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-# 1. Data Input Page ----
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-
+# Create 1. Input Data Main Tab
 shiny::tabPanel("1. Input Data", shiny::navlistPanel(
-
   id = "tabset",
-
   widths = sb_width,
 
-  ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-  ## 1. Data Input Choice Section ----
-  ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-
+  # 1. Data Input Choice Section ----
   source("2_UI_Scripts/2-01_dummy_data_choice.R",  local = TRUE)$value,
 
-  ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-  ## 2. File Upload Section ----
-  ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-
+  # 2. File Upload Section ----
   source("2_UI_Scripts/2-02_file_upload.R",  local = TRUE)$value,
 
-  ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-  ## 3. Data Process Section ----
-  ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-
+  # 3. Data Process Section ----
   source("2_UI_Scripts/2-03_var_convert.R",  local = TRUE)$value,
 
-  ### ### ### ### ### ### ### ### ### ### ### ### ### ###
   # 4. App Data Summary Section ----
-  ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-
   source("2_UI_Scripts/2-04_post_proc_summary.R",  local = TRUE)$value
-
   )
-
   )
 
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
