b4f03de5c5f674c01fb9dffc1244b944735839f0
rjmithcell92 <robertjames.mitchell@phs.scot>
2025-02-21 11:49:10 +0000

    Shortened Code for UI Script 2-03 for greater efficiency

--- inst/disclosure_control/2_UI_Scripts/2-03_var_convert.R
+++ inst/disclosure_control/2_UI_Scripts/2-03_var_convert.R
@@ -1,56 +1,34 @@
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
 #Run on Posit version: 4.1.2
-#Last updated: 02 Oct 2024
+#Last updated: 21 Feb 2025
 #By: Robert Mitchell
 #Script: 2-03_var_convert.R
-#Purpose: To set up UI for the Variable Conversion Sub Tab
+#Purpose: Set up UI for the Sub-Tab used to convert variable types
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
 
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-# 1. Data Process - Variable Conversion ----
-### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
+# Create Variable Conversion Sub Tab for 1. Input Data Main Tab
 shiny::tabPanel("Variable Conversion",
 
-                ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-                ## 1. Input and output Definitions ----
-                ### ### ### ### ### ### ### ### ### ### ### ### ### ###
-
+                # Input and Output Definitions
                 shiny::fluidRow(
 
-                  ### ### ### ### ### ### ### ###
-                  ### 1. Sidebar panel for inputs ----
-                  ### ### ### ### ### ### ### ###
-
+                  # Input Definitions
                   shiny::column(3,
-
-                  # Choose Variable to Convert
-                  shiny::selectInput("Variable_Convert", label = "Choose Variable to Convert:", choices = "", multiple = FALSE),
-
-                  shiny::br(),
-
-                  #Button to convert character variable to numeric variable
-                  shiny::h5(shiny::strong("Convert Character Variable to Numeric Variable")),
-                  shiny::actionButton('char_to_num', 'Convert'),
-
-                  shiny::br(),
-
-                  #Button to convert numeric variable to character variable
-                  shiny::h5(shiny::strong("Convert Numeric Variable to Character Variable")),
-                  shiny::actionButton('num_to_char', 'Convert')
-
+                                shiny::selectInput("Variable_Convert", label = "Choose Variable to Convert:", choices = "", multiple = FALSE), # Variable Conversion Options
+                                shiny::br(), # New Line
+                                shiny::h5(shiny::strong("Convert Character Variable to Numeric Variable")), # Sub Heading for Character to Numeric Variable Conversion
+                                shiny::actionButton('char_to_num', 'Convert'), # Button for convert character variables to numeric variables
+                                shiny::br(), # New Line
+                                shiny::h5(shiny::strong("Convert Numeric Variable to Character Variable")), # Sub Heading for Numeric to Character Variable Conversion
+                                shiny::actionButton('num_to_char', 'Convert') # Button for convert numeric variables to character variables
                   ),
 
-                  ### ### ### ### ### ### ### ###
-                  ### 2. Output Display ----
-                  ### ### ### ### ### ### ### ###
-
-                  shiny::mainPanel(DT::dataTableOutput("convert_data"))
-
-                  )
-
+                  # Output Definitions
+                  shiny::mainPanel(DT::dataTableOutput("convert_data")) # Visualisation of Processed Data in App
                 )
+)
 
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
 ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ###
