33848809b9d672cea17b3b56e58382612e8ab295
rjmithcell92 <robertjames.mitchell@phs.scot>
2025-03-05 16:18:43 +0000

    Tests for primary suppression function

--- tests/testthat/test-disclosure_functions.R
+++ tests/testthat/test-disclosure_functions.R
@@ -17,7 +17,7 @@ r_sample_data <- data.frame(
 testthat::test_that("Valid inputs", {
   r_vars <- c("20-24", "25-29", "30-34", "35-39", "40-44", "45-49", "50-54", "55-59", "Total")
   r_base <- 3
-  r_data <- Stat_Round(r_sample_data, r_vars, r_base)
+  r_data <- sdcshinyapp::Stat_Round(r_sample_data, r_vars, r_base)
 
   testthat::expect_equal(r_data$`20-24`, c(NA, 0, 24))
   testthat::expect_equal(r_data$`25-29`, c(9, NA, 27))
@@ -31,22 +31,77 @@ testthat::test_that("Valid inputs", {
 })
 
 testthat::test_that("Invalid orig_data", {
-  testthat::expect_error(Stat_Round(NULL, c("20-24"), 3), "Error: 'orig_data' must be provided, cannot be NULL, and must be a dataframe or tibble.")
-  testthat::expect_error(Stat_Round(list(), c("20-24"), 3), "Error: 'orig_data' must be provided, cannot be NULL, and must be a dataframe or tibble.")
+  testthat::expect_error(sdcshinyapp::Stat_Round(NULL, c("20-24"), 3), "Error: 'orig_data' must be provided, cannot be NULL, and must be a dataframe or tibble.")
+  testthat::expect_error(sdcshinyapp::Stat_Round(list(), c("20-24"), 3), "Error: 'orig_data' must be provided, cannot be NULL, and must be a dataframe or tibble.")
 })
 
 testthat::test_that("Invalid var_choice", {
-  testthat::expect_error(Stat_Round(r_sample_data, NULL, 3), "Error: 'var_choice' must be a non-empty character vector and all columns must exist in 'orig_data'.")
-  testthat::expect_error(Stat_Round(r_sample_data, c("nonexistent"), 3), "Error: 'var_choice' must be a non-empty character vector and all columns must exist in 'orig_data'.")
+  testthat::expect_error(sdcshinyapp::Stat_Round(r_sample_data, NULL, 3), "Error: 'var_choice' must be a non-empty character vector and all columns must exist in 'orig_data'.")
+  testthat::expect_error(sdcshinyapp::Stat_Round(r_sample_data, c("nonexistent"), 3), "Error: 'var_choice' must be a non-empty character vector and all columns must exist in 'orig_data'.")
 })
 
 testthat::test_that("Invalid round_cond", {
-  testthat::expect_error(Stat_Round(r_sample_data, c("20-24"), -10), "Error: 'round_cond' must be a single positive whole integer.")
-  testthat::expect_error(Stat_Round(r_sample_data, c("20-24"), 0), "Error: 'round_cond' must be a single positive whole integer.")
-  testthat::expect_error(Stat_Round(r_sample_data, c("20-24"), 10.5), "Error: 'round_cond' must be a single positive whole integer.")
+  testthat::expect_error(sdcshinyapp::Stat_Round(r_sample_data, c("20-24"), -10), "Error: 'round_cond' must be a single positive whole integer.")
+  testthat::expect_error(sdcshinyapp::Stat_Round(r_sample_data, c("20-24"), 0), "Error: 'round_cond' must be a single positive whole integer.")
+  testthat::expect_error(sdcshinyapp::Stat_Round(r_sample_data, c("20-24"), 10.5), "Error: 'round_cond' must be a single positive whole integer.")
 })
 
 testthat::test_that("No numeric variables selected", {
   r_vars <- c("line")
-  testthat::expect_warning(Stat_Round(r_sample_data, r_vars, 3), "No numeric variables have been selected. The original input data will be returned.")
+  testthat::expect_warning(sdcshinyapp::Stat_Round(r_sample_data, r_vars, 3), "No numeric variables have been selected. The original input data will be returned.")
+})
+
+# Primary Suppression Test ----
+
+# Sample data for testing
+ps_sample_data <- data.frame(
+  `20-24` = c(1, 2, 3, 4, 5),
+  `25-29` = c(0, 1, 2, 3, 4),
+  `30-34` = c(5, 6, 7, 8, 9),
+  `35-39` = c(NA, 2, 3, 4, 5),
+  `40-44` = c(1, 2, 3, 4, 5),
+  `45-49` = c(0, 0, 0, 0, 0),
+  `50-54` = c(1, 2, 3, 4, 5),
+  `55-59` = c(1, 2, 3, 4, 5),
+  `Total` = c(8, 15, 21, 27, 33),
+  check.names = FALSE, stringsAsFactors = FALSE
+  )
+
+testthat::test_that("Function handles non-dataframe input", {
+  testthat::expect_error(sdcshinyapp::Stat_Primary_Supress(NULL, c("20-24"), "*", 3, TRUE), "Error: 'orig_data' must be a dataframe or tibble.")
+  testthat::expect_error(sdcshinyapp::Stat_Primary_Supress(list(), c("20-24"), "*", 3, TRUE), "Error: 'orig_data' must be a dataframe or tibble.")
+})
+
+testthat::test_that("Function handles non-existent columns", {
+  testthat::expect_error(sdcshinyapp::Stat_Primary_Supress(ps_sample_data, c("nonexistent"), "*", 3, TRUE), "Error: 'var_choice' must be a non-empty character vector and all columns must exist in 'orig_data'.")
+})
+
+# testthat::test_that("Function handles invalid suppression character", {
+#   testthat::expect_error(sdcshinyapp::Stat_Primary_Supress(ps_sample_data, c("20-24"), "x", 3, TRUE), "Error: 'char_supp' must be either '*' or 'c'.")
+# })
+
+testthat::test_that("Function handles invalid suppression condition", {
+  testthat::expect_error(sdcshinyapp::Stat_Primary_Supress(ps_sample_data, c("20-24"), "*", -1, TRUE), "Error: 'sup_cond' must be a single positive whole integer.")
+})
+
+testthat::test_that("Function handles invalid zero suppression flag", {
+  testthat::expect_error(sdcshinyapp::Stat_Primary_Supress(ps_sample_data, c("20-24"), "*", 3, "yes"), "Error: 'zero' must be a single logical value.")
+})
+
+testthat::test_that("Function suppresses values correctly", {
+  result <- sdcshinyapp::Stat_Primary_Supress(ps_sample_data, c("20-24", "25-29"), "*", 3, TRUE)
+  expected <- ps_sample_data
+  expected$`20-24`[1:3] <- "*"
+  expected$`25-29`[2:4] <- "*"
+  testthat::expect_equal(result, expected)
+})
+
+testthat::test_that("Function preserves NA values", {
+  result <- sdcshinyapp::Stat_Primary_Supress(ps_sample_data, c("35-39"), "*", 3, TRUE)
+  testthat::expect_true(is.na(result$`35-39`[1]))
+})
+
+testthat::test_that("Function returns original data if no numeric variables are selected", {
+  result <- Stat_Primary_Supress(ps_sample_data, c("Total"), "*", 3, TRUE)
+  testthat::expect_equal(result, ps_sample_data)
 })
